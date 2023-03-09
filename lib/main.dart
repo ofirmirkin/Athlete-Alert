5c767c2f65e263426a2ae1485a03d0ceeef041b0
cullenoi <cullenoi@tcd.ie>
2023-03-09 19:37:24 +0000

    as

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:software_design/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
