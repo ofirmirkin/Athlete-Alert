cc8d25a954d4377728dfbe8641a5120c94a5c89e
cullenoi <cullenoi@tcd.ie>
2023-03-09 20:41:01 +0000

    fuck servers

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:software_design/login_page.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
