d2fb2c24382a8de00a9ca1bf7e4c618b014307f8
cullenoi <cullenoi@tcd.ie>
2023-03-13 18:22:02 +0000

    Confirming forgot password added

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -37,141 +37,140 @@ class _LoginPageState extends State<LoginPage> {
         backgroundColor: Colors.blueGrey,
         body: SafeArea(
             child: Center(
-          child: SingleChildScrollView(
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              // ignore: prefer_const_constructors
-              Icon(
-                Icons.add_location_alt_sharp,
-                size: 90,
+                child: SingleChildScrollView(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            // ignore: prefer_const_constructors
+            Icon(
+              Icons.add_location_alt_sharp,
+              size: 90,
+            ),
+            // ignore: prefer_const_constructors
+            SizedBox(height: 100),
+            //TODO Touch up spacing and colour theme for phone version
+            //Welcome
+            Text(
+              'Welcome',
+              style: GoogleFonts.bebasNeue(
+                fontSize: 36,
               ),
-              // ignore: prefer_const_constructors
-              SizedBox(height: 100),
-              //TODO Touch up spacing and colour theme for phone version
-              //Welcome
-              Text(
-                'Welcome',
-                style: GoogleFonts.bebasNeue(
-                  fontSize: 36,
+            ),
+
+            //login or signup button
+            // IN login page will have a button which leads to forgot password @kru3ish
+            // ignore: prefer_const_constructors
+            SizedBox(height: 15),
+
+            Text(
+              'Great to see your alive and well',
+              style: TextStyle(fontSize: 20),
+            ),
+            //Signup Button Goes to @KUNAL
+            SizedBox(height: 25),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 158, 233, 233),
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 18.0),
+                  child: TextField(
+                    controller: _emailController,
+                    // at the moment theres no limit to the
+                    //character so limit will go off screen
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Email',
+                    ),
+                  ),
                 ),
               ),
+            ),
 
-              //login or signup button
-              // IN login page will have a button which leads to forgot password @kru3ish
-              // ignore: prefer_const_constructors
-              SizedBox(height: 15),
+            SizedBox(height: 25),
 
-              Text(
-                'Great to see your alive and well',
-                style: TextStyle(fontSize: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 158, 233, 233),
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 18.0),
+                  child: TextField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    // at the moment theres no limit to the
+                    //character so limit will go off screen
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Password',
+                    ),
+                  ),
+                ),
               ),
-              //Signup Button Goes to @KUNAL
-              SizedBox(height: 25),
+            ),
 
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            SizedBox(height: 25),
+
+            //SizedBox(height: 25),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 150.0),
+              child: GestureDetector(
+                onTap: logIn,
                 child: Container(
+                  padding: EdgeInsets.all(20),
                   decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 158, 233, 233),
+                    color: Colors.grey,
                     border: Border.all(color: Colors.white),
                     borderRadius: BorderRadius.circular(12),
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 18.0),
-                    child: TextField(
-                      controller: _emailController,
-                      // at the moment theres no limit to the
-                      //character so limit will go off screen
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Email',
-                      ),
-                    ),
+                  child: Center(
+                    child: Text('Log In',
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold)),
                   ),
                 ),
               ),
+            ),
 
-              SizedBox(height: 25),
+            SizedBox(height: 25),
 
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 158, 233, 233),
-                    border: Border.all(color: Colors.white),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 18.0),
-                    child: TextField(
-                      controller: _passwordController,
-                      obscureText: true,
-                      // at the moment theres no limit to the
-                      //character so limit will go off screen
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Password',
-                      ),
-                    ),
-                  ),
-                ),
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Dont Have an Account?',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold)),
+                Text(' Signup Now',
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 2, 128, 232),
+                        fontWeight: FontWeight.bold)),
+              ],
+            ),
 
-              SizedBox(height: 25),
+            SizedBox(height: 25),
 
-              //SizedBox(height: 25),
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 150.0),
-                child: GestureDetector(
-                  onTap: logIn,
-                  child: Container(
-                    padding: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      border: Border.all(color: Colors.white),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Center(
-                      child: Text('Log In',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold)),
-                    ),
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 25),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Dont Have an Account?',
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold)),
-                  Text(' Signup Now',
-                      style: TextStyle(
-                          color: Color.fromARGB(255, 2, 128, 232),
-                          fontWeight: FontWeight.bold)),
-                ],
-              ),
-
-              SizedBox(height: 25),
-
-             Row(
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 GestureDetector(
                   onTap: () {
-                    Navigator.push(context,
-                      MaterialPageRoute(builder: (context) {
-                        return ForgotPasswordPage();
-                      },
-
-              ),
-              );
-              },
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return ForgotPasswordPage();
+                        },
+                      ),
+                    );
+                  },
                   child: Text(
                     'Forgot Password?',
                     style: TextStyle(
@@ -179,10 +178,10 @@ class _LoginPageState extends State<LoginPage> {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-            ),
-            ],
+                ),
+              ],
             ),
           ]),
-        )));
+        ))));
   }
 }
