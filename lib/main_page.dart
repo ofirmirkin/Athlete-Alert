101470ef1e767bfb4cdb876cb9f34c5815b48aea
Ofir MIRKIN <60558136+ofirmirkin@users.noreply.github.com>
2023-04-13 17:26:33 +0100

    Tried to add the phone page on first login

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -4,23 +4,43 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:never_surf_alone/login_page.dart';
 import 'map.dart';
+import 'phoneDB.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'home_page.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
+  firstTime() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool firstTime = prefs.getBool('first_time') ?? true;
+    if (firstTime) {
+      prefs.setBool('first_time', false);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: StreamBuilder<User?>(
-            stream: FirebaseAuth.instance.authStateChanges(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return MapSample();
-              } else {
-                return LoginPage();
-              }
-            }));
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            if (firstTime()) {
+              print("first time");
+              return EnterPhoneNum();
+            } else {
+              return MapSample();
+            }
+          } else {
+            return LoginPage();
+          }
+        },
+      ),
+    );
   }
 }
