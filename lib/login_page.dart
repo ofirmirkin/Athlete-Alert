aaf24967d02092a9a436b3b4283dfcff4ae51100
Brian Barrett <Brian Barrett>
2023-04-14 00:02:27 +0100

    Phone Number Request On Signup

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'forgot-password.dart';
+import 'signup.dart';
 import 'package:geolocator/geolocator.dart';
 
 class LoginPage extends StatefulWidget {
@@ -46,174 +47,171 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        //backgroundColor: Colors.indigoAccent[700],
-         backgroundColor: Color.fromRGBO(47, 36, 255, 1),
+        backgroundColor: Colors.blueGrey,
         body: SafeArea(
             child: Center(
-          child: SingleChildScrollView(
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              // ignore: prefer_const_constructors
-              // Icon(
-              //   Icons.add_location_alt_outlined,
-              //   color: Colors.white,
-              //   size: 90,
-              // ),
-               Image.asset('assets/images/logo.png'),// height: 222 , width: 300),
-              // ignore: prefer_const_constructors
-              SizedBox(height: 15),
-              //TODO Touch up spacing and colour theme for phone version
-              //Welcome
-              Text(
-                'Welcome',
-                style: GoogleFonts.bebasNeue(color: Colors.white,
-                  fontSize: 36,
+                child: SingleChildScrollView(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            // ignore: prefer_const_constructors
+            Icon(
+              Icons.add_location_alt_sharp,
+              size: 90,
+            ),
+            // ignore: prefer_const_constructors
+            SizedBox(height: 100),
+            //TODO Touch up spacing and colour theme for phone version
+            //Welcome
+            Text(
+              'Welcome',
+              style: GoogleFonts.bebasNeue(
+                fontSize: 36,
+              ),
+            ),
+
+            //login or signup button
+            // IN login page will have a button which leads to forgot password @kru3ish
+            // ignore: prefer_const_constructors
+            SizedBox(height: 15),
+
+            Text(
+              'Great to see your alive and well',
+              style: TextStyle(fontSize: 20),
+            ),
+            //Signup Button Goes to @KUNAL
+            SizedBox(height: 25),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 158, 233, 233),
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(12),
                 ),
-              ),
-
-              //login or signup button
-              // IN login page will have a button which leads to forgot password @kru3ish
-              // ignore: prefer_const_constructors
-              SizedBox(height: 15),
-
-              Text(
-                'Great to see youre alive and well',
-                style: TextStyle(fontSize: 15, color: Colors.white),
-              ),
-              //Signup Button Goes to @KUNAL
-              SizedBox(height: 15),
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    
-                     color: Colors.white,
-                    border: Border.all(color: Colors.blueAccent),
-                    borderRadius: BorderRadius.circular(12),
-                    
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 22.0),
-                    child: TextField(
-                      controller: _emailController,
-                      // at the moment theres no limit to the
-                      //character so limit will go off screen
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Email',
-                      ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 18.0),
+                  child: TextField(
+                    controller: _emailController,
+                    // at the moment theres no limit to the
+                    //character so limit will go off screen
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Email',
                     ),
                   ),
                 ),
               ),
+            ),
 
-              SizedBox(height: 15),
+            SizedBox(height: 25),
 
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    //color: Color.fromARGB(255, 158, 233, 233),
-                    color: Colors.white,
-                    border: Border.all(color: Colors.blueAccent),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 22.0),
-                    child: TextField(
-                      controller: _passwordController,
-                      obscureText: true,
-                      // at the moment theres no limit to the
-                      //character so limit will go off screen
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Password',
-                      ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 158, 233, 233),
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 18.0),
+                  child: TextField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    // at the moment theres no limit to the
+                    //character so limit will go off screen
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Password',
                     ),
                   ),
                 ),
               ),
+            ),
 
-              SizedBox(height: 10),
+            SizedBox(height: 25),
 
-              Row(
-                //mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
+            //SizedBox(height: 25),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 150.0),
+              child: GestureDetector(
+                onTap: logIn,
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Center(
+                    child: Text('Log In',
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold)),
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 25),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Dont Have an Account?',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold)),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return SignUpPage();
+                        },
+                      ),
+                    );
+                  },
+                  child: Text(
+                    'Sign Up Now',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 25),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
                           return ForgotPasswordPage();
                         },
-
-                ),
-                );
-            
-                }, child: Padding(
-                     //alignment: Alignment.centerLeft,
-                     padding:const EdgeInsets.only(left: 220.0),
-                    child: Text(
-                      'Forgot your Password?',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
                       ),
-                    ),
-              ),
-                  ),
-              ],
-              
-              ),
-
-
-              SizedBox(height: 15),
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 150.0),
-                child: GestureDetector(
-                  onTap: logIn,
-                  child: Container(
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Colors.blueAccent,
-                      border: Border.all(color: Colors.white),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Center(
-                      child: Text('Log In',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold)),
+                    );
+                  },
+                  child: Text(
+                    'Forgot Password?',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-              ),
-
-              SizedBox(height: 15),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Dont Have an Account?  ',
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold)),
-                  Text('Signup Now',
-                      style: TextStyle(
-                          color: Colors.white,
-                          decoration: TextDecoration.underline,
-                          fontWeight: FontWeight.bold)),
-                ],
-              ),
-
-              SizedBox(height: 15),
-
-           
-     
+              ],
+            ),
           ]),
         ))));
   }
-}
   // --------------- Ask for location permission -----------------
 
   Future<Position> determinePosition() async {
@@ -240,4 +238,4 @@ class _LoginPageState extends State<LoginPage> {
 
     return await Geolocator.getCurrentPosition();
   }
-
+}
